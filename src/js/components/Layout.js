12564325bee715edc65d7c4c2fe915b81b82f6cb
t0yohei <inuneko690121@gmail.com>
2020-06-14 14:45:38 +0900

    state と props の組み合わせを実装

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,12 +3,20 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = {title: "Welcome"};
+  }
+
   render() {
-    const title = "Welcome Tsutomu!";
+   setTimeout(
+     () => { this.setState({title: "Welcome Tsutomu!"}); },
+     2000
+   );
 
     return (
       <div>
-        <Header title={title} />
+        <Header title={this.state.title} />
         <Header title={"Thank you"} />
         <Footer />
       </div>
