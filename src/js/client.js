55d3e146fe5e3a3d51e7096dce5da55ddee65a0a
t0yohei <inuneko690121@gmail.com>
2020-06-14 13:52:40 +0900

    コンストラクターを使用して初期化処理を実装

--- src/js/client.js
+++ src/js/client.js
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Layout extends React.Component {
+  constructor() {
+    super();
+    this.name = "Tsutomu";
+  }
+
   render() {
     return (
-      <h1>It works!</h1>
+      <h1>It's {this.name}!</h1>
     );
   }
 }
