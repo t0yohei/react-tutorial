df30937edb6e07a0febba5c5526d77ed34de016b
t0yohei <inuneko690121@gmail.com>
2020-06-14 14:06:34 +0900

    header, footer のコンポーネントを作成

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,13 +1,14 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
 export default class Layout extends React.Component {
-  constructor() {
-    super();
-    this.name = "Tsutomu";
-  }
   render() {
     return (
-      <h1>It's {this.name}!</h1>
+      <div>
+        <Header />
+        <Footer />
+      </div>
     );
   }
 }
